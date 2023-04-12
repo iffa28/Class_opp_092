835a123f689b37f4c4ded613c3b94ee4b321dd73
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-04-12 10:41:03 +0700

    membuat class Mahasiswa

--- Classopp092/Classopp092.cpp
+++ Classopp092/Classopp092.cpp
@@ -10,6 +10,34 @@ public:
     void input() {
         cout << "Masukkan NIM : ";
         cin >> nim;
+        cout << "Masukkan Nama : ";
+        cin >> nama;
+        cout << "Masukkan Nilai : ";
+        cin >> nilai;
+    }
+    void printData();
+};
+
+void Mahasiswa::printData() {
+    cout << "\nData Mahasiswa";
+    cout << "\nNIM : " << nim;
+    cout << "\nNamanya : " << nama;
+    cout << "\nNilai : " << nilai;
+
+}
+class matakuliah {
+private:
+    string kode;
+    string namaMk;
+    int sks;
+public:
+    void inputMK() {
+        cout << "\n\nMasukkan kode matakuliah : ";
+        cin >> kode;
+        cout << "Masukkan Nama matakuliah : ";
+        cin >> namaMk;
+        cout << "Masukkan SKS : ";
+        cin >> sks;
     }
 };
 int main()
